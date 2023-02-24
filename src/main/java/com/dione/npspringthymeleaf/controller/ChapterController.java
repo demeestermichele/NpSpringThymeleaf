3f8d1bbc6a80c07842d1f305896d411887c2c7a9
Michele de Meester <demeestermichele@gmail.com>
2023-02-24 16:29:59 +0100

    chapter list works, top bar link to chapter dashboard

--- src/main/java/com/dione/npspringthymeleaf/controller/ChapterController.java
+++ src/main/java/com/dione/npspringthymeleaf/controller/ChapterController.java
@@ -1,9 +1,91 @@
 package com.dione.npspringthymeleaf.controller;
 
+import com.dione.npspringthymeleaf.model.Chapter;
+import com.dione.npspringthymeleaf.model.Charac;
+import com.dione.npspringthymeleaf.model.EventDate;
+import com.dione.npspringthymeleaf.model.EventType;
+import com.dione.npspringthymeleaf.repository.ChapterRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.List;
 
 @Controller
 @RequestMapping("/chapter")
 public class ChapterController {
+
+    @Autowired
+    private ChapterRepository repository;
+
+    @GetMapping(value = "/all")
+    public String showAll(Model model){
+        List<Chapter> chapterList = repository.findAll(Sort.by("book"));
+        model.addAttribute("chapters", chapterList);
+        return "chapter/chapter-list";
+    }
+
+    @GetMapping(value = "/error")
+    public String errorPage(Model model){
+        Iterable<Chapter> list = repository.findAll();
+        return "redirect:/index";
+    }
+
+
+    @GetMapping(value = "/create")
+    public String showCreateForm(Model model) {
+        Chapter chapter = new Chapter();
+        System.out.println("CREATE CHAPTER");
+        model.addAttribute("chapters", chapter);
+        return "chapter/chapter-creation";
+    }
+
+
+    @PostMapping("/update/{id}")
+    public String updateChapter(@PathVariable("id") long id, @Valid Chapter chapter, BindingResult result, Model model) {
+        if (result.hasErrors()) {
+            chapter.setId(id);
+            System.out.println("something went wrong with update");
+            return "error";
+        }
+        repository.save(chapter);
+        model.addAttribute("chapter", repository.findChaptersById(id));
+        return "chapter/chapter-profile";
+    }
+
+
+    @GetMapping("/edit/{id}")
+    public String updateForm(@PathVariable("id") long id, Model model) {
+        model.addAttribute("chapter", repository.findChaptersById(id));
+        return "chapter/chapter-edit";
+    }
+
+    @GetMapping("/{id}")
+    public String chapterProfile(@PathVariable("id") long id, Model model) {
+        Chapter chapter = repository.findChaptersById(id);
+        model.addAttribute("chapter", chapter);
+        model.addAttribute("all", repository.findAll());
+//        System.out.println(eventDateList.listIterator().next().getShortForm());
+        return "chapter/chapter-profile";
+    }
+
+    @GetMapping("/delete/{id}")
+    public String deleteUser(@PathVariable("id") long id, Model model) {
+        Chapter chapter = repository.findChaptersById(id);
+        repository.delete(chapter);
+        return "redirect:/chapter/all";
+    }
+
+    @PostMapping(value = "/save") //persists
+    public String saveCharac(@ModelAttribute Chapter form, Model model) {
+        repository.save(form);
+        model.addAttribute("chapter", repository.findChaptersById(form.getId()));
+
+        return "redirect:/chapter/all";
+    }
+
 }
